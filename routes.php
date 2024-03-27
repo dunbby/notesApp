4b4383f3b4fd6ae60ab7b2161d4a083a63b9218d
dunbby <yoyordanov94@gmail.com>
2024-03-27 10:59:25 +0200

    add a middleware

--- routes.php
+++ routes.php
@@ -1,18 +1,21 @@
 <?php
 
+// main pages
 $router->get('/', 'controllers/index.php');
 $router->get('/about', 'controllers/about.php');
 $router->get('/contact', 'controllers/contact.php');
 
-$router->get('/notes', 'controllers/notes/index.php');
-$router->get('/note', 'controllers/notes/show.php');
-$router->get('/note/edit', 'controllers/notes/edit.php');
-$router->get('/note/create', 'controllers/notes/create.php');
+// note views
+$router->get('/notes', 'controllers/notes/index.php')->only('auth');
+$router->get('/note', 'controllers/notes/show.php')->only('auth');
+$router->get('/note/edit', 'controllers/notes/edit.php')->only('auth');
+$router->get('/note/create', 'controllers/notes/create.php')->only('auth');
 
+// note actions
 $router->post('/note', 'controllers/notes/store.php');
 $router->patch('/note', 'controllers/notes/update.php');
 $router->delete('/note', 'controllers/notes/destroy.php');
 
-$router->get('/register', 'controllers/registration/create.php');
-
+// register
+$router->get('/register', 'controllers/registration/create.php')->only('guest');
 $router->post('/register', 'controllers/registration/store.php');
