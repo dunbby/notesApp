8d3ee2de6717f1e38ea48bcfbbef8e58a410223c
dunbby <yoyordanov94@gmail.com>
2024-03-27 15:42:17 +0200

    extract form validation and authenticator

--- routes.php
+++ routes.php
@@ -1,26 +1,26 @@
 <?php
 
 // main pages
-$router->get('/', 'controllers/index.php');
-$router->get('/about', 'controllers/about.php');
-$router->get('/contact', 'controllers/contact.php');
+$router->get('/', 'index.php');
+$router->get('/about', 'about.php');
+$router->get('/contact', 'contact.php');
 
 // note views
-$router->get('/notes', 'controllers/notes/index.php')->only('auth');
-$router->get('/note', 'controllers/notes/show.php')->only('auth');
-$router->get('/note/edit', 'controllers/notes/edit.php')->only('auth');
-$router->get('/note/create', 'controllers/notes/create.php')->only('auth');
+$router->get('/notes', 'notes/index.php')->only('auth');
+$router->get('/note', 'notes/show.php')->only('auth');
+$router->get('/note/edit', 'notes/edit.php')->only('auth');
+$router->get('/note/create', 'notes/create.php')->only('auth');
 
 // note actions
-$router->post('/note', 'controllers/notes/store.php')->only('auth');
-$router->patch('/note', 'controllers/notes/update.php')->only('auth');
-$router->delete('/note', 'controllers/notes/destroy.php')->only('auth');
+$router->post('/note', 'notes/store.php')->only('auth');
+$router->patch('/note', 'notes/update.php')->only('auth');
+$router->delete('/note', 'notes/destroy.php')->only('auth');
 
 // register
-$router->get('/register', 'controllers/registration/create.php')->only('guest');
-$router->post('/register', 'controllers/registration/store.php')->only('guest');
+$router->get('/register', 'registration/create.php')->only('guest');
+$router->post('/register', 'registration/store.php')->only('guest');
 
-// login
-$router->get('/login', 'controllers/session/create.php')->only('guest');
-$router->post('/session', 'controllers/session/store.php')->only('guest');
-$router->delete('/session', 'controllers/session/destroy.php')->only('auth');
+// login & logout
+$router->get('/login', 'session/create.php')->only('guest');
+$router->post('/session', 'session/store.php')->only('guest');
+$router->delete('/session', 'session/destroy.php')->only('auth');
